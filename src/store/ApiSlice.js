544f41e4078b62a6f6fd33c218c59c27c7fb9840
Tushar Kautkar <prajwalgadg9899@gmail.com>
2022-07-01 21:53:25 +0530

    second commit

--- src/store/ApiSlice.js
+++ src/store/ApiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseUri = 'http://localhost:5000'
+const baseUri = 'https://expense--api.herokuapp.com/'
 
 export const ApiSlice = createApi({
     
